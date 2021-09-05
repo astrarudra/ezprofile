effd53add699dbe74a54ada524d13f437dda88dd
Rudra Roy <43406144+astrarudra@users.noreply.github.com>
2021-09-06 02:58:00 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,31 +1,28 @@
 // config.js
 module.exports = {
     github: {
-        username: 'arifszn', // Your GitHub org/user name. (Required)
+        username: 'astrarudra', // Your GitHub org/user name. (Required)
         sortBy: 'stars', // stars | updated
-        limit: 8, // How many projects to display.
+        limit: 2, // How many projects to display.
         exclude: {
             forks: false, // Forked projects will not be displayed if set to true.
             projects: [] // These projects will not be displayed. example: ['my-project1', 'my-project2']
         }
     },
     social: {
-        linkedin: 'ariful-alam',
+        linkedin: 'rudraroy1507',
         twitter: 'arif_swozon',
-        facebook: '',
+        facebook: 'astrarudra',
         dribbble: '',
         behance: '',
         medium: '',
         devto: '',
-        website: 'https://arifszn.github.io',
-        email: 'contact@arifszn.com'
+        website: 'http://royin.in',
+        email: 'rudraroy1507@gmail.com'
     },
     skills: [
-        'PHP',
-        'Laravel',
         'JavaScript',
         'React.js',
-        'Vue.js',
         'Node.js',
         'Jquery',
         'MySQL',
@@ -37,56 +34,50 @@ module.exports = {
     ],
     experiences: [
         { 
-            company: 'Monstarlab Bangladesh',
-            position: 'Software Engineer',
-            from: 'September 2021',
+            company: 'Ipsos MMA',
+            position: 'Senior Software Engineer',
+            from: 'September 2018',
             to: 'Present'
         },
         { 
-            company: 'Orangetoolz - My Offer 360 Degree',
-            position: 'Jr. Full Stack Engineer',
-            from: 'July 2019',
-            to: 'August 2021'
+            company: 'Amazon, IN',
+            position: 'QA Engineer',
+            from: 'May 2018',
+            to: 'September 2017'
         },
-        { 
-            company: 'Techvillage',
-            position: 'Jr. Software Engineer',
-            from: 'January 2019',
-            to: ' June 2019'
-        }
     ],
     education: [
         { 
-            institution: 'American International University-Bangladesh',
-            degree: 'Bachelor of Science',
-            from: '2015',
-            to: '2019'
+            institution: 'SRM University, KTR Chennai',
+            degree: 'B.Tech - Bachelor of Technilogy',
+            from: '2018',
+            to: '2014'
         },
         { 
-            institution: 'Cantonment College, Jessore',
-            degree: 'Higher Secondary Certificate (HSC)',
+            institution: 'Delhi Public School, Ruby Park, Kolkata',
+            degree: 'Higher Secondary Certificate (CSBC)',
             from: '2012',
             to: '2014',
         },
         { 
-            institution: 'Chowgacha Shahadat Pilot High School',
-            degree: 'Secondary School Certificate (SSC)',
-            from: '2007',
+            institution: 'Don Bosco, Park Circus, Kolkata',
+            degree: 'Secondary School Certificate (ICSE)',
+            from: '2000',
             to: '2012'
         }
     ],
     blog: {
         // Display blog posts from your medium or dev.to account. (Optional)
-        source: 'dev.to', // medium | dev.to
-        username: 'arifszn',
-        limit: 3 // How many posts to display. Max is 10.
+        // source: 'dev.to', // medium | dev.to
+        // username: 'arifszn',
+        // limit: 3 // How many posts to display. Max is 10.
     },
     googleAnalytics: {
         // GA3 tracking id/GA4 tag id
         id: '' // UA-XXXXXXXXX-X | G-XXXXXXXXXX
     },
     themeConfig: {
-        default: 'light',
+        default: 'dark',
 
         // Hides the switch in the navbar
         // Useful if you want to support a single color mode
